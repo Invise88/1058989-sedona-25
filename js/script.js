a57317331f06a1e2c476907a8c5e400a741f8ef0
Invise88 <50359361+Invise88@users.noreply.github.com>
2019-06-23 16:18:27 +0300

    исправления

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var link = document.querySelector(".appointment-button");
 
- var popup = document.querySelector(".appointment-form");
-
- link.addEventListener("click", function (evt) {
-   evt.preventDefault();
-   popup.classList.toggle("appointment-form-show");
- });
+var popup = document.querySelector(".appointment-form");
+popup.classList.add("appointment-form-hide");
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.toggle("appointment-form-hide");
+});
